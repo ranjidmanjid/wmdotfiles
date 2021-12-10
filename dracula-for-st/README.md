6f205117700dfe473e874518ada58980c5ab7f96
Daniel Marley <dinkara21@gmail.com>
2021-12-10 22:11:48 +0000

    inital config commit

---
+++ dracula-for-st/README.md
@@ -0,0 +1,30 @@
+# Dracula for [st](http://st.suckless.org/)
+
+> A dark theme for the simple terminal program [st](http://st.suckless.org/).
+
+![Screenshot](screenshot.png)
+
+## Install
+
+1. Download **st-dracula-20170803-7f99032.diff** and save it anywhere on your computer.
+
+## Activating theme
+
+1. Downlad st source from [the official repository](http://dl.suckless.org/st/).
+2. Unzip and navigate to the st directory.
+3. `git apply /path/to/the/patch/st-dracula-20170803-7f99032.diff`.
+4. Compile st as usual.
+
+## Caveats
+
+Notice that the only way for chaning the colorscheme on st is to recompile it.
+
+## Team
+
+This theme is maintained by the following person(s) and a bunch of [awesome contributors](https://github.com/dracula/template/graphs/contributors).
+
+[![Jesús López](https://avatars0.githubusercontent.com/u/18138817?v=4&s=70)](https://github.com/jelohe) 
+
+## License
+
+[MIT License](./LICENSE)
---
+++ slstatus/config.h
@@ -0,0 +1,88 @@
+/* See LICENSE file for copyright and license details. */
+
+/* interval between updates (in ms) */
+const unsigned int interval = 1000;
+
+/* text to show if no value can be retrieved */
+static const char unknown_str[] = "n/a";
+
+/* maximum output string length */
+#define MAXLEN 2048
+
+/*
+ * function            description                     argument (example)
+ *
+ * battery_perc        battery percentage              battery name (BAT0)
+ *                                                     NULL on OpenBSD/FreeBSD
+ * battery_state       battery charging state          battery name (BAT0)
+ *                                                     NULL on OpenBSD/FreeBSD
+ * battery_remaining   battery remaining HH:MM         battery name (BAT0)
+ *                                                     NULL on OpenBSD/FreeBSD
+ * cpu_perc            cpu usage in percent            NULL
+ * cpu_freq            cpu frequency in MHz            NULL
+ * datetime            date and time                   format string (%F %T)
+ * disk_free           free disk space in GB           mountpoint path (/)
+ * disk_perc           disk usage in percent           mountpoint path (/)
+ * disk_total          total disk space in GB          mountpoint path (/")
+ * disk_used           used disk space in GB           mountpoint path (/)
+ * entropy             available entropy               NULL
+ * gid                 GID of current user             NULL
+ * hostname            hostname                        NULL
+ * ipv4                IPv4 address                    interface name (eth0)
+ * ipv6                IPv6 address                    interface name (eth0)
+ * kernel_release      `uname -r`                      NULL
+ * keyboard_indicators caps/num lock indicators        format string (c?n?)
+ *                                                     see keyboard_indicators.c
+ * keymap              layout (variant) of current     NULL
+ *                     keymap
+ * load_avg            load average                    NULL
+ * netspeed_rx         receive network speed           interface name (wlan0)
+ * netspeed_tx         transfer network speed          interface name (wlan0)
+ * num_files           number of files in a directory  path
+ *                                                     (/home/foo/Inbox/cur)
+ * ram_free            free memory in GB               NULL
+ * ram_perc            memory usage in percent         NULL
+ * ram_total           total memory size in GB         NULL
+ * ram_used            used memory in GB               NULL
+ * run_command         custom shell command            command (echo foo)
+ * separator           string to echo                  NULL
+ * swap_free           free swap in GB                 NULL
+ * swap_perc           swap usage in percent           NULL
+ * swap_total          total swap size in GB           NULL
+ * swap_used           used swap in GB                 NULL
+ * temp                temperature in degree celsius   sensor file
+ *                                                     (/sys/class/thermal/...)
+ *                                                     NULL on OpenBSD
+ *                                                     thermal zone on FreeBSD
+ *                                                     (tz0, tz1, etc.)
+ * uid                 UID of current user             NULL
+ * uptime              system uptime                   NULL
+ * username            username of current user        NULL
+ * vol_perc            OSS/ALSA volume in percent      mixer file (/dev/mixer)
+ *                                                     NULL on OpenBSD
+ * wifi_perc           WiFi signal in percent          interface name (wlan0)
+ * wifi_essid          WiFi ESSID                      interface name (wlan0)
+ */
+
+static const char get_weather[] = "curl -s --connect-timeout 1 \
+                                   curl wttr.in/Wiesbaden?format=3 \
+                                   | awk -F \"+\" '{print $2}'";
+
+static const char get_weather_full[] = "curl -s --connect-timeout 1 \
+                                   		curl curl wttr.in/Wiesbaden?format=%C+%t+%w \
+                                   		| awk -F \"+\" '{print $1 $2}'";                               
+                                   
+static const char get_price[] = "curl -s http://api.coindesk.com/v1/bpi/currentprice.json  | jq -r '.bpi.USD.rate'";
+/* static const char get_price[] = "curl -s http://api.coindesk.com/v1/bpi/currentprice.json | python -c 'import json, sys; print(json.load(sys.stdin)['bpi']['USD']['rate'])'";*/
+
+static const struct arg args[] = {
+	/* function           format            		   argument */
+  /*{ battery_perc,       "[%s%]",         			   "%F %T" },*/
+  /*{ temp,               "[%sC]",         			   "%F %T" },*/
+  /*{ ram_total,          "[%s%]",         			   "%F %T" },*/
+	{ run_command,  	  "[  %s$]",     			 get_price },
+	{ run_command,  	  "[  %s]",    		  get_weather_full },
+	{ cpu_perc,           "[CPU %s%]",      		   "%F %T" },
+    { ram_perc,           "[RAM %s%]",      		   "%F %T" },
+	{ datetime,           "[%s]",           		   "%F %T" },
+};
